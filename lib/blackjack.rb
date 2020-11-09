6d0aa719a2abb70ef767645990dc383b92681c3a
David Wisecarver <dwisecar@gmail.com>
2020-11-09 20:23:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ end
 def hit?(card_total)
    prompt_user
    user_input = get_user_input
-   puts user_input
+   puts user_input == "h"
    if user_input == 'h'
      puts "Input is H"
      card_total += deal_card
